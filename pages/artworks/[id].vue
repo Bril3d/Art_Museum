1a2a1024ece3b98877f4a021423aac3011dae48c
Bril3d <khaledtbk@gmail.com>
2023-01-15 14:37:09 +0100

    Handeling 404

--- pages/artworks/[id].vue
+++ pages/artworks/[id].vue
@@ -5,8 +5,12 @@
 </template>
 
 <script setup>
+const router = useRouter();
 const { id } = useRoute().params
 const { data: artwork } = await useFetch(`https://api.artic.edu/api/v1/artworks/${id}`,{key:id})
+if(!artwork.value){
+    throw createError({statusCode:404, statusMessage:'Artwork Not Found'})
+}
 </script>
 
 <style scoped>
